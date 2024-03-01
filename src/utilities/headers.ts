dbba21e249eaf99ad32c94835140e7e4a35f2ec2
lepzulnag <gindre.matthieu@gmail.com>
2024-03-01 22:19:44 +0100

    🐛 Allow CORS everywhere

--- src/utilities/headers.ts
+++ src/utilities/headers.ts
@@ -1,8 +1,3 @@
-import { adminKey } from "./adminKey"
-
-export const headers: HeadersInit = {}
-
-if (adminKey == "toto") {
-	// in local mode, allow every origin
-	headers["Access-Control-Allow-Origin"] = "*"
+export const headers: HeadersInit = {
+	["Access-Control-Allow-Origin"]: "*",
 }
